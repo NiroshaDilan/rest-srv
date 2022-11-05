c8028c82c585ee59061a1d1607555dc8d5b16c62
NiroshaDilan <dilan.vista@gmail.com>
2022-11-05 17:20:27 +0530

    extended by servlet initializer

--- src/main/java/lk/icoder/restsrv/RestSrvApplication.java
+++ src/main/java/lk/icoder/restsrv/RestSrvApplication.java
@@ -2,12 +2,18 @@ package lk.icoder.restsrv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestSrvApplication {
+public class RestSrvApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestSrvApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestSrvApplication.class);
+    }
 }
--- src/main/java/lk/icoder/restsrv/controller/HelloWorldController.java
+++ src/main/java/lk/icoder/restsrv/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloWorldController {
 
-    @GetMapping
+    @GetMapping("/healthcheck")
     public String healthCheck() {
         return "Service is working...";
     }
